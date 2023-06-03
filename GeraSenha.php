90b99172b4c5fa134ab30c20ccd209d04248743a
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2023-06-03 11:41:42 -0300

    Add files via upload

---
+++ GeraSenha.php
@@ -0,0 +1,28 @@
+<?php
+	//***********************************************************************************************
+	// AUTOR: Ricardo Erick Rebêlo
+	// Objetivo: "rolar" dados dos RPGs de mesa mais 
+	// Versão Original (Pastebin https://pastebin.com/Vj16ynRh): 02/07/2014 - Ricardo Erick Rebêlo
+	// Alterações:
+	// 0.1   21/04/2023 - Conversão
+	// 1.0   21/04/2023 - Publicação
+
+	namespace jacknpoe;
+
+	//***********************************************************************************************
+	// Classe DungeonsAndDragos (D&D)
+
+	class GeraSenha
+	{
+
+		function GeraSenha( $tamanho = 10)
+		{
+			$acumulador = "";
+			for( $indice = 0; $indice < $tamanho; $indice++)
+			{
+				$acumulador .= chr( rand( 33, 126));
+			}
+			return $acumulador;
+		}
+	}
+?>
---
+++ php_testegerasenha.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<title>Teste de Classe \jacknpoe\GeraSenha (senhas com números, maiúsculas, minúsculas e símbolos)</title>
+ 		<link rel="stylesheet" href="php_testegerasenha.css"/>
+		<link rel="icon" type="image/png" href="php_testegerasenha.png"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<?php
+			header( "Content-Type: text/html; charset=ISO-8859-1", true);
+
+			$tamanho = '';
+			$resultado = '';
+
+			if( isset( $_POST[ 'gerar']))
+			{
+				require_once( 'GeraSenha.php');
+				$GeradorSenha = new \jacknpoe\GeraSenha();
+
+				$tamanho =  $_POST['tamanho'];
+
+				$resultado = 'Senha: ' . $GeradorSenha->GeraSenha( (int)$tamanho);
+			}
+		?>
+		<h1>Gerador de Senhas</h1><br>
+
+		<form action="php_testegerasenha.php" method="POST" style="border: 0px">
+			<p>Tamanho: <input type="text" name="tamanho" value="<?php echo htmlspecialchars( $tamanho, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML401, "ISO-8859-1"); ?>" style="width: 50px" autofocus></p>
+			<p><input type="submit" name="gerar" value="Gerar"></p>
+		</form><br>
+		<p><?php echo $resultado; ?></p>
+		<br>
+
+		<p><a href="https://github.com/jacknpoe/php_testegerasenha">Repositório no GitHub</a></p><br><br>
+		<form action="index.html" method="POST" style="border: 0px">
+			<p><input type="submit" name="voltar" value="Voltar"></p>
+		</form>
+	</body>
+</html>
